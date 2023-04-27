c551637be4af6314c41f3d3f44fef120aaffe68b
Otávio <otaviogrocha@yahoo.com.br>
2023-04-27 01:11:43 -0300

    creating component footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FullBannerComponent } from './components/full-banner/full-banner.component';
 import { ListTitlesComponent } from './components/list-titles/list-titles.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FullBannerComponent,
-    ListTitlesComponent
+    ListTitlesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
