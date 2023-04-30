3cc2d751c71e25a1170be936322045512af3da24
Otávio <otaviogrocha@yahoo.com.br>
2023-04-30 00:23:14 -0300

    also using @Input decorator to receive the property value from the list-titles template, replacing the movie (title) template with the info received from the list declared at the app.cp.ts -> no longer static + use of [] to declare a variable value instead of static + directives ngFor and ngClass to define the categories and which age would be according to the class

--- src/app/components/titles/titles.component.ts
+++ src/app/components/titles/titles.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-titles',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./titles.component.css']
 })
 export class TitlesComponent {
+
+  @Input() title: any;
 
 }
