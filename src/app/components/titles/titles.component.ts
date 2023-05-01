e04871c5e7908ae6656515e6a6870b7547bc53e4
Otávio <otaviogrocha@yahoo.com.br>
2023-05-01 13:51:38 -0300

    adding mouse enter and leave events to the video + new icons to mute the video using NgIf directive to select which icon will be used according to the muted property and using # to identify and use View Child decorator. Altered icons css  + new funcs to play and stop video

--- src/app/components/titles/titles.component.ts
+++ src/app/components/titles/titles.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-titles',
@@ -8,5 +8,28 @@ import { Component, Input } from '@angular/core';
 export class TitlesComponent {
 
   @Input() title: any;
+  @ViewChild('trailer', { static: true }) trailer!: ElementRef<HTMLVideoElement> ;
+
+  constructor() { }
+
+  ngOnInit(){
+  }
+
+  playVideo(){
+    this.trailer.nativeElement.play()
+  }
+  
+  stopVideo(){
+    this.trailer.nativeElement.currentTime = 0;
+    this.trailer.nativeElement.pause();
+  }
+
+  getMuted(): boolean{
+    return this.trailer.nativeElement.muted;
+  }
+
+  toggleSound(){
+    this.trailer.nativeElement.muted = !this.trailer.nativeElement.muted;
+  }
 
 }
